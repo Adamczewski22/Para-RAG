90b4b57ad012061b8c4897ef2049efd7130b3d7b
Szymon Adamczewski <s.a.adamczewski@student.vu.nl>
2026-04-21 11:51:27 +0200

    embeddings implemented

---
+++ app/ai/embeddings.py
@@ -0,0 +1,14 @@
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_core.embeddings import Embeddings
+from dotenv import find_dotenv, load_dotenv
+from functools import lru_cache
+
+load_dotenv(find_dotenv())
+
+EMBEDDINGS_MODEL = "text-embedding-3-large"
+
+@lru_cache(maxsize=1)
+def get_embeddings() -> Embeddings:
+    return OpenAIEmbeddings(
+        model=EMBEDDINGS_MODEL,
+    )
