e662b1ba4a9a965995c05c50efb53a448955bed2
Szymon Adamczewski <s.a.adamczewski@student.vu.nl>
2026-05-08 16:02:53 +0200

    feat: add script to convert locomo json to shape supported by memobase rag baseline

---
+++ benchmarks/locomo_json_rag_converter.py
@@ -0,0 +1,71 @@
+import argparse
+import json
+
+
+def main(dataset_path: str, output_path: str):
+    with open(file=dataset_path, mode="r", encoding="utf-8") as file:
+        data = json.load(file)
+    
+    sample = data[1] # Choose the second QA task
+    sample_id = sample["sample_id"]
+    qa = sample["qa"]
+    conversation = sample["conversation"]
+    
+    # Flatten the conversation by merging the sessions
+    conversation_flat = []
+    session_index = 1
+    while True:
+        session_name = f"session_{session_index}"
+        # Break if there are no more sessions
+        if session_name not in conversation:
+            break
+        
+        timestamp = conversation[f"{session_name}_date_time"]
+        session = conversation[session_name]
+
+        # Iterate over messages
+        for msg in session:
+            conversation_flat.append(
+                {
+                    "timestamp": timestamp,
+                    "speaker": msg["speaker"],
+                    "text": msg["text"],
+                }
+            )
+        session_index += 1
+
+    # Skip adversarial questions
+    qa_no_adversarial = [item for item in qa if item["category"] != 5]
+
+    result = {
+        sample_id: {
+            "conversation": conversation_flat,
+            "question": qa_no_adversarial,
+        }
+    }
+
+    with open(file=output_path, mode="w", encoding="utf-8") as file:
+        json.dump(result, file, indent=2, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--dataset-path",
+        type=str,
+        default="data/locomo/locomo10.json",
+        help="Path to locomo benchmark json file",
+    )
+    parser.add_argument(
+        "--output-path",
+        type=str,
+        default="data/locomo/locomo10_rag.json",
+        help="Path to the output file to store the converted json",
+    )
+    args = parser.parse_args()
+
+    main(
+        dataset_path=args.dataset_path,
+        output_path=args.output_path,
+    )
